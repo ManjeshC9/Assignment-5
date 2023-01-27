5e8a6cf0868b3f04befddb654af1412d81ec0fdb
ManjeshC9 <120353457+ManjeshC9@users.noreply.github.com>
2023-01-27 22:57:27 +0530

    q4.py

---
+++ q4.py
@@ -0,0 +1,19 @@
+# """Write a Python program to construct the following pattern, using a nested for loop.
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+
+for i in range(5):
+    for j in range(i):
+        print ('* ', end="")
+    print('')
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print('* ', end="")
+    print('')
